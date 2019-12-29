f72ea9e459333a33a0f398d568c70cd3848ce449
hiteshkr759 <hiteshkr759@gmail.com>
2019-12-29 21:41:14 +0530

    connected to database

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'twt-me.clxck6b53h74.ap-south-1.rds.amazonaws.com',
-  user     : 'admin',
+  host     : 'hardrock.clxck6b53h74.ap-south-1.rds.amazonaws.com',
+  user     : 'root',
   password : 'Hitesh8797',
-  database : 'twt-me',
-  port: 3306
+  database : 'twitterApi',
+  port: 3306,
 });
 
 connection.connect(function(err) {
@@ -12,8 +12,14 @@ connection.connect(function(err) {
       console.error('Database connection failed: ' + err.stack);
       return;
     }
-  
     console.log('Connected to database.');
+    // connection.query('create database ',(err,res)=>{
+    //   console.log(err);
+    //   if(err) throw err;
+    //   console.log('Database created');
+    // });
+  
+    
   });
   
   connection.end();
